01eeef954e037c919ad51dda92c5110aa3a77e5a
Sergey Belyaev <sergey.belyaev@w3.co>
2017-04-04 13:01:37 +0300

    wpml_lang_custom_switcher.php updated

--- wpml_lang_custom_switcher.php
+++ wpml_lang_custom_switcher.php
@@ -2,7 +2,9 @@
 	<?php $languages = icl_get_languages(); ?>	
 	<ul>
 		<?php foreach ( $languages as $lang ) : ?>
-			<li<?php if ( $lang['active'] ) : ?> class="active"<?php endif; ?>><a href="<?php echo $lang['url']; ?>"><?php echo $lang['native_name']; ?></a></li>
+			<li<?php if ( $lang['active'] ) : ?> class="active"<?php endif; ?>>
+				<a href="<?php echo $lang['url']; ?>"><?php echo $lang['native_name']; ?></a>
+			</li>
 		<?php endforeach; ?>
 	</ul>
 <?php endif; ?>

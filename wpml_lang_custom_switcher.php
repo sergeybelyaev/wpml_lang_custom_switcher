63438ad44f1abad0921ef31b1e7a4c48925946c2
Sergey Belyaev <sergeybelya@gmail.com>
2021-03-25 21:07:45 +0200

    ul class, change name to code, link title

--- wpml_lang_custom_switcher.php
+++ wpml_lang_custom_switcher.php
@@ -1,9 +1,11 @@
 <?php if ( function_exists( 'icl_get_languages' ) ) : ?>
 	<?php $languages = icl_get_languages(); ?>	
-	<ul>
+	<ul class="languages-links">
 		<?php foreach ( $languages as $lang ) : ?>
 			<li<?php if ( $lang['active'] ) : ?> class="active"<?php endif; ?>>
-				<a href="<?php echo $lang['url']; ?>"><?php echo $lang['native_name']; ?></a>
+				<a href="<?php echo esc_url( $lang['url'] ); ?>" title="<?php echo esc_attr( $lang['native_name'] ); ?>">
+					<?php echo $lang['code']; ?>
+				</a>
 			</li>
 		<?php endforeach; ?>
 	</ul>
